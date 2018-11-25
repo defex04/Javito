b7eded58ecae4729e3d8d617c991270eca476b81
andrey <andrey.nagornov@hotmail.com>
2018-11-25 23:59:22 +0300

    Update #Javito5_2

--- javito/src/main/java/com/db/javito/JavitoApplication.java
+++ javito/src/main/java/com/db/javito/JavitoApplication.java
@@ -1,39 +1,26 @@
 package com.db.javito;
 
-import com.db.javito.model.EurPredict;
-import com.db.javito.model.Main;
-import com.db.javito.service.interf.EurPredictService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.db.javito.dao.interf.MainDao;
 import com.db.javito.service.interf.MainService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class JavitoApplication {
 
     @Autowired
-    MainService mainService;
-    EurPredictService eurPredictService;
+    public static MainService mainService;
 
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(JavitoApplication.class, args);
 		System.out.println("Javito");
-        MainService mainService = context.getBean(MainService.class);
-        Main main = new Main();
-        main.setTime("2018");
-        main.setGbp_rate(25.5f);
-        main.setEur_rate(3523.5f);
-        main.setUsd_rate(36.8f);
-        mainService.insert(main);
 
-        EurPredictService eurPredictService = context.getBean(EurPredictService.class);
-        EurPredict eurPredict = new EurPredict();
-        eurPredict.setPredict_val_eur(44.3f);
-        eurPredict.setIncrease_decrease(true);
-        eurPredictService.insert(eurPredict);
+        mainService = context.getBean(MainService.class);
+
     }
 }
