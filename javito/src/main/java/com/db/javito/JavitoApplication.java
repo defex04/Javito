7bb522eb0580b71ed10dc4e89a65ba0a7acea0ea
andrey <andrey.nagornov@hotmail.com>
2018-11-25 21:05:09 +0300

    Рефакторинг кода #Javito5_2

--- javito/src/main/java/com/db/javito/JavitoApplication.java
+++ javito/src/main/java/com/db/javito/JavitoApplication.java
@@ -1,14 +1,11 @@
 package com.db.javito;
 
-import com.db.javito.model.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.db.javito.dao.interf.MainDao;
 import com.db.javito.service.interf.MainService;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class JavitoApplication {
@@ -22,14 +19,6 @@ public class JavitoApplication {
 		System.out.println("Javito");
 
         mainService = context.getBean(MainService.class);
-
-        Main main = new Main();
-        main.setTime("2018");
-        main.setGbp_rate(25.5f);
-        main.setEur_rate(3523.5f);
-        main.setUsd_rate(36.8f);
-
-        //mainService.insert(main);
 
     }
 }
