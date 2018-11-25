42f122a48ce1f76e461c6a115314179e4022879e
MariiaKalugina <masha_k_o@mail.ru>
2018-11-25 17:44:55 +0300

    Change registers in schema-postgresql.sql

--- javito/src/main/resources/schema-postgresql.sql
+++ javito/src/main/resources/schema-postgresql.sql
@@ -1,48 +1,47 @@
-CREATE TABLE public."Main"
+CREATE TABLE public."main"
 (
     id integer NOT NULL,
     "time" text COLLATE pg_catalog."default" NOT NULL,
-    "USD_rate" real NOT NULL,
-    "GBP_rate" real NOT NULL,
-    "EUR_rate" real NOT NULL,
-    CONSTRAINT "Main_pkey" PRIMARY KEY (id)
+    "usd_rate" real NOT NULL,
+    "gbp_rate" real NOT NULL,
+    "eur_rate" real NOT NULL,
+    CONSTRAINT "main_pkey" PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."Main"
+ALTER TABLE public."main"
     OWNER to postgres;
 
-CREATE TABLE public."EUR_predict"
+CREATE TABLE public."eur_predict"
 (
     id_predict integer NOT NULL,
-    "predict_val_EUR" real NOT NULL,
+    "predict_val_eur" real NOT NULL,
     increase_decrease boolean NOT NULL,
-    CONSTRAINT "EUR_predict_pkey" PRIMARY KEY (id_predict)
+    CONSTRAINT "eur_predict_pkey" PRIMARY KEY (id_predict)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."EUR_predict"
+ALTER TABLE public."eur_predict"
     OWNER to postgres;
 
-
-CREATE TABLE public."EUR_id_predict"
+CREATE TABLE public."eur_id_predict"
 (
-    "id_EUR_predict" integer NOT NULL,
-    "id_EUR" integer NOT NULL,
+    "id_eur_predict" integer NOT NULL,
+    "id_eur" integer NOT NULL,
     id_predict integer NOT NULL,
-    CONSTRAINT "EUR_id_predict_pkey" PRIMARY KEY ("id_EUR_predict"),
-    CONSTRAINT "EUR_id_predict_id_EUR_fkey" FOREIGN KEY ("id_EUR")
-        REFERENCES public."Main" (id) MATCH SIMPLE
+    CONSTRAINT "eur_id_predict_pkey" PRIMARY KEY ("id_eur_predict"),
+    CONSTRAINT "eur_id_predict_id_eur_fkey" FOREIGN KEY ("id_eur")
+        REFERENCES public."main" (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "EUR_id_predict_id_predict_fkey" FOREIGN KEY (id_predict)
-        REFERENCES public."EUR_predict" (id_predict) MATCH SIMPLE
+    CONSTRAINT "eur_id_predict_id_predict_fkey" FOREIGN KEY (id_predict)
+        REFERENCES public."eur_predict" (id_predict) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -51,44 +50,44 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."EUR_id_predict"
+ALTER TABLE public."eur_id_predict"
     OWNER to postgres;
 
 -- Table: public."GBP_predict"
 
 -- DROP TABLE public."GBP_predict";
 
-CREATE TABLE public."GBP_predict"
+CREATE TABLE public."gbp_predict"
 (
     id_predict integer NOT NULL,
-    "predict_val_GBP" real NOT NULL,
+    "predict_val_gbp" real NOT NULL,
     increase_decrease boolean NOT NULL,
-    CONSTRAINT "GBP_predict_pkey" PRIMARY KEY (id_predict)
+    CONSTRAINT "gbp_predict_pkey" PRIMARY KEY (id_predict)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."GBP_predict"
+ALTER TABLE public."gbp_predict"
     OWNER to postgres;
 
 -- Table: public."GBP_id_predict"
 
 -- DROP TABLE public."GBP_id_predict";
 
-CREATE TABLE public."GBP_id_predict"
+CREATE TABLE public."gbp_id_predict"
 (
-    "id_GBP_predict" integer NOT NULL,
-    "id_GBP" integer NOT NULL,
+    "id_gbp_predict" integer NOT NULL,
+    "id_gbp" integer NOT NULL,
     id_predict integer NOT NULL,
-    CONSTRAINT "GBP_id_predict_pkey" PRIMARY KEY ("id_GBP_predict"),
-    CONSTRAINT "GBP_id_predict_id_GBP_fkey" FOREIGN KEY ("id_GBP")
-        REFERENCES public."Main" (id) MATCH SIMPLE
+    CONSTRAINT "gbp_id_predict_pkey" PRIMARY KEY ("id_gbp_predict"),
+    CONSTRAINT "gbp_id_predict_id_gbp_fkey" FOREIGN KEY ("id_gbp")
+        REFERENCES public."main" (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "GBP_id_predict_id_predict_fkey" FOREIGN KEY (id_predict)
-        REFERENCES public."GBP_predict" (id_predict) MATCH SIMPLE
+    CONSTRAINT "gbp_id_predict_id_predict_fkey" FOREIGN KEY (id_predict)
+        REFERENCES public."gbp_predict" (id_predict) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -97,42 +96,42 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."GBP_id_predict"
+ALTER TABLE public."gbp_id_predict"
     OWNER to postgres;
 
-- Table: public."USD_predict";
+- Table: public."usd_predict";
 
-CREATE TABLE public."USD_predict"
+CREATE TABLE public."usd_predict"
 (
-    "predict_val_USD" real NOT NULL,
+    "predict_val_usd" real NOT NULL,
     increase_decrease boolean NOT NULL,
     id_predict integer NOT NULL,
-    CONSTRAINT "USD_predict_pkey" PRIMARY KEY (id_predict)
+    CONSTRAINT "usd_predict_pkey" PRIMARY KEY (id_predict)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."USD_predict"
+ALTER TABLE public."usd_predict"
     OWNER to postgres;
 
 -- Table: public."USD_id_predict"
 
 -- DROP TABLE public."USD_id_predict";
 
-CREATE TABLE public."USD_id_predict"
+CREATE TABLE public."usd_id_predict"
 (
-    "id_USD_predict" integer NOT NULL,
-    "id_USD" integer NOT NULL,
+    "id_usd_predict" integer NOT NULL,
+    "id_usd" integer NOT NULL,
     id_predict integer NOT NULL,
-    CONSTRAINT "USD_id_predict_pkey" PRIMARY KEY ("id_USD_predict"),
-    CONSTRAINT "USD_id_predict_id_USD_predict_fkey" FOREIGN KEY ("id_USD_predict")
-        REFERENCES public."Main" (id) MATCH SIMPLE
+    CONSTRAINT "usd_id_predict_pkey" PRIMARY KEY ("id_usd_predict"),
+    CONSTRAINT "usd_id_predict_id_usd_predict_fkey" FOREIGN KEY ("id_usd_predict")
+        REFERENCES public."main" (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "USD_id_predict_id_predict_fkey" FOREIGN KEY (id_predict)
-        REFERENCES public."USD_predict" (id_predict) MATCH SIMPLE
+    CONSTRAINT "usd_id_predict_id_predict_fkey" FOREIGN KEY (id_predict)
+        REFERENCES public."usd_predict" (id_predict) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -141,19 +140,19 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."USD_id_predict"
+ALTER TABLE public."usd_id_predict"
     OWNER to postgres;
 
 -- Table: public."Day"
 
 -- DROP TABLE public."Day";
 
-CREATE TABLE public."Day"
+CREATE TABLE public."day"
 (
     id integer NOT NULL,
     id_day integer NOT NULL,
-    CONSTRAINT "Day_pkey" PRIMARY KEY (id),
-    CONSTRAINT "Day_id_day_key" UNIQUE (id_day)
+    CONSTRAINT "day_pkey" PRIMARY KEY (id),
+    CONSTRAINT "day_id_day_key" UNIQUE (id_day)
 
 )
 WITH (
@@ -161,22 +160,22 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."Day"
+ALTER TABLE public."day"
     OWNER to postgres;
 
 -- Table: public."EUR_min_max"
 
 -- DROP TABLE public."EUR_min_max";
 
-CREATE TABLE public."EUR_min_max"
+CREATE TABLE public."eur_min_max"
 (
     id integer NOT NULL,
     id_day integer NOT NULL,
-    "EUR_max" real NOT NULL,
-    "EUR_min" real NOT NULL,
-    CONSTRAINT "EUR_min_max_pkey" PRIMARY KEY (id),
-    CONSTRAINT "EUR_min_max_id_day_fkey" FOREIGN KEY (id_day)
-        REFERENCES public."Day" (id_day) MATCH SIMPLE
+    "eur_max" real NOT NULL,
+    "eur_min" real NOT NULL,
+    CONSTRAINT "eur_min_max_pkey" PRIMARY KEY (id),
+    CONSTRAINT "eur_min_max_id_day_fkey" FOREIGN KEY (id_day)
+        REFERENCES public."day" (id_day) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -185,22 +184,22 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."EUR_min_max"
+ALTER TABLE public."eur_min_max"
     OWNER to postgres;
 
 -- Table: public."GBP_min_max"
 
 -- DROP TABLE public."GBP_min_max";
 
-CREATE TABLE public."GBP_min_max"
+CREATE TABLE public."gbp_min_max"
 (
     id integer NOT NULL,
     id_day integer NOT NULL,
-    "GBP_max" real NOT NULL,
-    "GBP_min" real NOT NULL,
-    CONSTRAINT "GBP_min_max_pkey" PRIMARY KEY (id),
-    CONSTRAINT "GBP_min_max_id_day_fkey" FOREIGN KEY (id_day)
-        REFERENCES public."Day" (id_day) MATCH SIMPLE
+    "gbp_max" real NOT NULL,
+    "gbp_min" real NOT NULL,
+    CONSTRAINT "gbp_min_max_pkey" PRIMARY KEY (id),
+    CONSTRAINT "gbp_min_max_id_day_fkey" FOREIGN KEY (id_day)
+        REFERENCES public."day" (id_day) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -209,22 +208,22 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."GBP_min_max"
+ALTER TABLE public."gbp_min_max"
     OWNER to postgres;
 
 -- Table: public."USD_min_max"
 
 -- DROP TABLE public."USD_min_max";
 
-CREATE TABLE public."USD_min_max"
+CREATE TABLE public."usd_min_max"
 (
     id integer NOT NULL,
     id_day integer NOT NULL,
-    "USD_max" real NOT NULL,
-    "USD_min" real NOT NULL,
-    CONSTRAINT "USD_min_max_pkey" PRIMARY KEY (id),
-    CONSTRAINT "USD_min_max_id_day_fkey" FOREIGN KEY (id_day)
-        REFERENCES public."Day" (id_day) MATCH SIMPLE
+    "usd_max" real NOT NULL,
+    "usd_min" real NOT NULL,
+    CONSTRAINT "usd_min_max_pkey" PRIMARY KEY (id),
+    CONSTRAINT "usd_min_max_id_day_fkey" FOREIGN KEY (id_day)
+        REFERENCES public."day" (id_day) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -233,5 +232,5 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public."USD_min_max"
+ALTER TABLE public."usd_min_max"
     OWNER to postgres;
